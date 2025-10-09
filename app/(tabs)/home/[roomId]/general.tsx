1b0e90eeb307f6c2c9232c198d3dca8dd29e9256
Ahmad <itxti909@gmail.com>
2025-10-09 18:13:19 +0300

    general

--- app/(tabs)/home/[roomId]/general.tsx
+++ app/(tabs)/home/[roomId]/general.tsx
@@ -1,13 +1,41 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Card, CardContent } from "@/components/ui/card";
+import { Text } from "@/components/ui/text";
+import { useUserData } from "@/server/auth";
+import { useUsersRooms } from "@/server/usersRooms/get-users-rooms";
+import { useLocalSearchParams } from "expo-router";
+import React from "react";
+import { StyleSheet, View } from "react-native";
 
 const General = () => {
-    return (
-        <View>
-            
-        </View>
-    );
-}
+  const { roomId } = useLocalSearchParams<{ roomId: string }>();
+  const { data } = useUserData();
 
+  const { data: rooms } = useUsersRooms({
+    userId: data?.data.id || "",
+    queryConfig: { enabled: !!data?.data.id },
+  });
+
+  const currentRoom = rooms?.data.find(
+    (room) => room.room.id.toString() == roomId
+  );
+
+  console.log(currentRoom);
+  console.log(roomId);
+
+  return (
+    <View className="gap-4 p-4" style={{ flex: 1 }}>
+      <Card>
+        <CardContent>
+          <Text>{currentRoom?.room.name}</Text>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardContent>
+          <Text>{currentRoom?.room.roomNumber}</Text>
+        </CardContent>
+      </Card>
+    </View>
+  );
+};
 
 export default General;
